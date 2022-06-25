2ba8fb2c8c7ec14a159a2734e2fcb931d5bbbccf
debas-31 <debas31@gmail.com>
2022-06-25 14:02:14 +0300

    Modify database schema file

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_06_23_161701) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
